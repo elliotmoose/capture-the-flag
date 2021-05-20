c1d022386856defa602e78159c2e04a98079a465
elliotmoose <kyzelliot@gmail.com>
2021-05-20 15:47:06 +0800

    cleanup and init controls

--- Assets/Src/Controls/PlayerController.cs
+++ Assets/Src/Controls/PlayerController.cs
@@ -7,28 +7,20 @@ using MLAPI.Messaging;
 using MLAPI.Spawning;
 using MLAPI.NetworkVariable;
 using Cinemachine;
-public enum ControlType {
-    ThirdPerson,
-    TopDown
-}
-
 public class PlayerController : NetworkBehaviour 
 {   
+    NetworkVariableBool sprinting = new NetworkVariableBool(new NetworkVariableSettings{
+        WritePermission = NetworkVariablePermission.OwnerOnly
+    });
     NetworkVariableVector3 moveDir = new NetworkVariableVector3(new NetworkVariableSettings{
         WritePermission = NetworkVariablePermission.OwnerOnly,
         SendTickrate = 20,
-    });
-    
-    NetworkVariableVector3 navTarget = new NetworkVariableVector3(new NetworkVariableSettings{
-        WritePermission = NetworkVariablePermission.OwnerOnly
     });
     
     public NetworkVariableULong playerObjNetId = new NetworkVariableULong(new NetworkVariableSettings{
         WritePermission = NetworkVariablePermission.ServerOnly,
         SendTickrate = 0,        
     });
-
-    public ControlType controlType = ControlType.ThirdPerson;
 
     float commandDurationThreshold = 0.1f;
     float timeSinceLastCommand = 0f;
@@ -63,33 +55,17 @@ public class PlayerController : NetworkBehaviour
     void Update()
     {   
         if(IsLocalPlayer) {
-            switch (controlType)
-            {
-                case ControlType.ThirdPerson:
-                    float horizontal = Input.GetAxisRaw("Horizontal");
-                    float vertical = Input.GetAxisRaw("Vertical");
-                    
-                    Vector3 _moveDir = new Vector3(horizontal, 0, vertical).normalized;
-                    if(_moveDir.magnitude != 0) {
-                        float targetAngle = Mathf.Atan2(_moveDir.x, _moveDir.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-                        moveDir.Value  = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-                    }
-                    else {
-                        moveDir.Value = Vector3.zero;
-                    }
-                    break;
-                case ControlType.TopDown:
-                    if(Input.GetMouseButton(1)) {
-                        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                        RaycastHit hit;
-                        if(Physics.Raycast(ray, out hit)) {                    
-                            // GameObject go = hit.transform.gameObject;
-                            navTarget.Value = hit.point;
-                        }
-                    }
-                    break;
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            
+            Vector3 _moveDir = new Vector3(horizontal, 0, vertical).normalized;
+            if(_moveDir.magnitude != 0) {
+                float targetAngle = Mathf.Atan2(_moveDir.x, _moveDir.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+                moveDir.Value  = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
             }
-
+            else {
+                moveDir.Value = Vector3.zero;
+            }
         }
 
         if(IsServer) {
@@ -100,22 +76,9 @@ public class PlayerController : NetworkBehaviour
             // GameObject playerObj = GameObject.Find("player"+GetComponent<NetworkObject>().OwnerClientId.ToString());
             NetworkObject playerNetworkObj = NetworkSpawnManager.SpawnedObjects[playerObjNetId.Value];
             GameObject playerObj = playerNetworkObj.gameObject;
-
-            // Debug.Log(navTarget.Value);
             if(playerObj) {
-
-                switch (controlType)
-                {
-                    case ControlType.ThirdPerson:
-                        Player player = playerObj.GetComponent<Player>();
-                        player.moveDir = GetMoveDir();
-                        break;
-                    case ControlType.TopDown:
-                        NavMeshAgent navMeshAgent = playerObj.GetComponent<NavMeshAgent>();
-                        navMeshAgent.enabled = true;
-                        navMeshAgent.SetDestination(navTarget.Value);
-                        break;
-                }
+                Player player = playerObj.GetComponent<Player>();
+                player.moveDir = GetMoveDir();
             }
         }
     }
