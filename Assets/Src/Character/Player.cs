8666aa8fd3f66b4241d29526848dd9fc36eaa1f8
Elliot Koh <appdev.moose@gmail.com>
2021-05-25 13:11:50 +0800

    Added isdisabled and cleaned up player update method

--- Assets/Src/Character/Player.cs
+++ Assets/Src/Character/Player.cs
@@ -48,80 +48,40 @@ public class Player : NetworkBehaviour
        
     }
 
-    // Player() : base() {
-    //     Debug.Log("player constructed");
-    //     ownerClientId.OnValueChanged += (ulong oldId, ulong newId) => {
-    //         this.name = "player"+newId.ToString();
-    //         Debug.Log("onvaluechanged");
-
-    //         //set camera
-    //         if(newId == NetworkManager.Singleton.LocalClientId) {
-    //             Camera.main.transform.SetParent(this.transform);
-    //         }
-    //     };
-
-    //     Debug.Log("event subscribed");
-    // }
-
     // Update is called once per frame
 
     private float _animationTransitionTime = 0.15f;
     private float _curTransitionTime = 0f;
     void Update()
     {
-        if(IsServer) {
-            bool isMoving = (moveDir.magnitude > 0.01f);
-            bool canSprint = (curStamina > 0);
-            bool isSprinting = (canSprint && sprinting);
-            //Debug.Log(curStamina);
-            if(isMoving) {                
-                float moveDirAngle = Mathf.Atan2(moveDir.x, moveDir.y) * Mathf.Rad2Deg + faceAngle;
-                Vector3 positionDelta = Quaternion.Euler(0, moveDirAngle, 0) * Vector3.forward;
+        if(!IsServer) { return; }
 
-                if(isSprinting) {
-                    transform.position += positionDelta.normalized * Time.deltaTime * moveSpeed * sprintMultiplier;
-                    curStamina = Mathf.Max(0, curStamina - Time.deltaTime * staminaBurnFactor);
-                }
-                else {
-                    transform.position += positionDelta.normalized * Time.deltaTime * moveSpeed;
-                }
-            }
-            
-            //recover
-            if(!sprinting) {
-                curStamina = Mathf.Clamp(curStamina + Time.deltaTime * staminaRecoveryFactor, 0, maxStamina);
-            }
+        bool isMoving = (moveDir.magnitude > 0.01f && !isDisabled);
+        bool canSprint = (curStamina > 0 && isMoving);
+        bool isSprinting = (canSprint && sprinting);
+        //Debug.Log(curStamina);
+        if(isMoving) {                
+            float moveDirAngle = Mathf.Atan2(moveDir.x, moveDir.y) * Mathf.Rad2Deg + faceAngle;
+            Vector3 positionDelta = Quaternion.Euler(0, moveDirAngle, 0) * Vector3.forward;
 
-            Vector2 targetMoveDir = moveDir/(isSprinting ? 1 : 2);
-            //animation smoohting
-            Animator animator = GetComponent<Animator>();
-            Vector2 curMoveDir = new Vector2(animator.GetFloat("HorMovement"), animator.GetFloat("VertMovement")); //current movedir state
-
-            if(curMoveDir != targetMoveDir) {
-                // _lastMoveDir = curMoveDir;
-                _curTransitionTime += Time.deltaTime;
-                Vector2 newSmoothMoveDir = Vector2.Lerp(curMoveDir, targetMoveDir, _curTransitionTime/_animationTransitionTime);
-                animator.SetFloat("HorMovement", newSmoothMoveDir.x);
-                animator.SetFloat("VertMovement", newSmoothMoveDir.y);            
+            if(isSprinting) {
+                transform.position += positionDelta.normalized * Time.deltaTime * moveSpeed * sprintMultiplier;
+                curStamina = Mathf.Max(0, curStamina - Time.deltaTime * staminaBurnFactor);
             }
             else {
-                _curTransitionTime = 0;
-            }   
-            
-            animator.SetBool("IsMoving", isMoving);
-
-
-            //update cooldowns
-            cdTimer1 -= Time.deltaTime;
-            cdTimer2 -= Time.deltaTime;
+                transform.position += positionDelta.normalized * Time.deltaTime * moveSpeed;
+            }
+        }
+        
+        //recover
+        if(!sprinting && !isDisabled) {
+            curStamina = Mathf.Clamp(curStamina + Time.deltaTime * staminaRecoveryFactor, 0, maxStamina);
         }
 
-        UpdateEffects(); // update skill effects applied to player
-        if (this.ToString()=="TestSubject (TestSubject)")
-        {
-            //Debug.Log(this.moveSpeed);
-        }       
+        SetAnimationsSmooth(isMoving, isSprinting);
 
+        UpdateCooldowns(); //update cooldowns
+        UpdateEffects(); // update skill effects applied to player
     }
 
     public void CastSkillAtIndex(int index) {
@@ -196,9 +156,36 @@ public class Player : NetworkBehaviour
         }
     }
 
+    void UpdateCooldowns() {
+        cdTimer1 -= Time.deltaTime;
+        cdTimer2 -= Time.deltaTime;
+    }
+    
     public void SetDisabled(bool disabled)
     {
         this.isDisabled = disabled;
     }
 
+
+    private void SetAnimationsSmooth(bool isMoving, bool isSprinting) {
+        if(!IsServer) { return; }
+
+        Vector2 targetMoveDir = moveDir/(isSprinting ? 1 : 2);
+        //animation smoohting
+        Animator animator = GetComponent<Animator>();
+        Vector2 curMoveDir = new Vector2(animator.GetFloat("HorMovement"), animator.GetFloat("VertMovement")); //current movedir state
+
+        if(curMoveDir != targetMoveDir) {
+            // _lastMoveDir = curMoveDir;
+            _curTransitionTime += Time.deltaTime;
+            Vector2 newSmoothMoveDir = Vector2.Lerp(curMoveDir, targetMoveDir, _curTransitionTime/_animationTransitionTime);
+            animator.SetFloat("HorMovement", newSmoothMoveDir.x);
+            animator.SetFloat("VertMovement", newSmoothMoveDir.y);            
+        }
+        else {
+            _curTransitionTime = 0;
+        }           
+        
+        animator.SetBool("IsMoving", isMoving);
+    }
 }
