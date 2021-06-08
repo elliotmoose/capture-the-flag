3a1e051e717f5d204a40111ea340fb71beb6c7cf
huiwen99 <56622233+huiwen99@users.noreply.github.com>
2021-06-08 17:22:28 +0800

    implemented invis dissolving vfx

--- Assets/Src/Character/Player.cs
+++ Assets/Src/Character/Player.cs
@@ -27,9 +27,6 @@ public class Player : NetworkBehaviour
     protected float staminaRecoveryFactor = 30;
     protected bool isDisabled = false;
 
-    //skill variables
-    protected bool isInvis = false;
-    protected float invisAlpha = 0.3f;
     
 
     float sprintMultiplier = 2.4f;
@@ -112,42 +109,6 @@ public class Player : NetworkBehaviour
         }
 
         SetAnimationsSmooth(isMoving, isSprinting);
-
-        // check if player is invisible
-        if (isInvis)
-        {
-            for (int i = 0; i < this.rends.Length; i++)
-            {
-                Renderer rend = rends[i];
-                if (this.team == PlayerController.LocalInstance.GetPlayer().team)
-                {
-                    // if same team, appear transparent
-                    rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, invisAlpha);
-                }
-                else
-                {
-                    // if enemy team, appear invisible
-                    rend.enabled = false;
-                }
-            }
-        }
-        else
-        {
-            for (int i = 0; i < this.rends.Length; i++)
-            {
-                Renderer rend = rends[i];
-
-                // revert invisible effect
-                if (this.team == PlayerController.LocalInstance.GetPlayer().team)
-                {
-                    rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 1.0f);
-                }
-                else
-                {
-                    rend.enabled = true;
-                }
-            }
-        }
 
         UpdateCooldowns(); //update cooldowns
         UpdateEffects(); // update skill effects applied to player
@@ -249,10 +210,6 @@ public class Player : NetworkBehaviour
         this.isDisabled = disabled;
     }
 
-    public void SetInvis(bool invis)
-    {
-        this.isInvis = invis;
-    }
     
     public bool IsCatchable()
     {
--- Assets/Src/Skills/Invisibility.cs
+++ Assets/Src/Skills/Invisibility.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Invisibility : Skill
 {
-    private float duration = 3.0f;
+    private float duration = 4.0f;
 
     public Invisibility()
     {
@@ -21,22 +21,69 @@ public class Invisibility : Skill
 
 public class InvisEffect : Effect
 {
-
+    private Renderer[] rends;
+    private float animation_duration = 0.5f;
+    private float alphaValue = 0.3f; // how transparent does your team see you
     public InvisEffect(Player _target, float duration) : base(_target)
     {
         this.duration = duration;
+        
         this.name = "INVIS_EFFECT";
-                
+        this.rends = _target.GetComponentsInChildren<Renderer>();
+
     }
 
     public override void OnEffectApplied()
     {
-        _target.SetInvis(true);
+        for (int i = 0; i < this.rends.Length; i++)
+        {
+            Renderer rend = rends[i];
+            if (_target.team == PlayerController.LocalInstance.GetPlayer().team)
+            {
+                // if same team, appear transparent
+                rend.material.SetFloat("_alphaValue", alphaValue);
+            }
+            else
+            {
+                // if enemy team, appear invisible
+                rend.material.SetFloat("_alphaValue", 0f);
+
+            }
+        }
+
     }
+
+    public override void UpdateEffect()
+    {
+        if (age <= animation_duration)
+        {
+            float progress = age / animation_duration;
+
+            for (int i = 0; i < this.rends.Length; i++)
+            {
+                Renderer rend = rends[i];
+                rend.material.SetFloat("_dissolved", progress);
+            }
+        }
+        else if (age >= duration - animation_duration)
+        {
+            float progress = (age - (duration - animation_duration)) / animation_duration;
+            for (int i = 0; i < this.rends.Length; i++)
+            {
+                Renderer rend = rends[i];
+                rend.material.SetFloat("_dissolved", 1-progress);
+            }
+        }
+    }
+
 
     public override void OnEffectEnd()
     {
-        _target.SetInvis(false);
+        for (int i = 0; i < this.rends.Length; i++)
+        {
+            Renderer rend = rends[i];
+            rend.material.SetFloat("_alphaValue", 1.0f);
+        }
     }
 
 }
