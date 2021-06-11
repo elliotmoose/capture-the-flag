8eaaf45e86674d87b59b31b3e29ea91a8ba6b6eb
Elliot Koh <appdev.moose@gmail.com>
2021-06-11 17:59:20 +0800

    prevent players from entering own end zone

--- Assets/Src/Game/EndZone.cs
+++ Assets/Src/Game/EndZone.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EndZone : MonoBehaviour
 {
     public Team team = Team.BLUE;
+    float endZoneSize = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,15 @@ public class EndZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        List<Player> players = PlayerSpawner.Instance.GetAllPlayers();
+        List<Player> teamPlayers = players.FindAll((Player player)=>player.GetTeam() == team);           
+        foreach(Player player in teamPlayers) {
+            Vector3 centerToPlayer = (player.transform.position-this.transform.position);
+            if(centerToPlayer.magnitude < endZoneSize) {
+                Vector3 newPos = this.transform.position + centerToPlayer.normalized*endZoneSize;
+                player.transform.position = new Vector3(newPos.x, player.transform.position.y, newPos.z);
+            }
+        }
     }
 
     void OnTriggerEnter(Collider hit) {
