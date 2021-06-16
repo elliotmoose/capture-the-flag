230adbfe255b8682d0cfaa7966f446b25561ddcd
Elliot Koh <appdev.moose@gmail.com>
2021-06-16 19:36:13 +0800

    cleaned up invis code

--- Assets/Src/Skills/Invisibility.cs
+++ Assets/Src/Skills/Invisibility.cs
@@ -21,69 +21,27 @@ public class Invisibility : Skill
 
 public class InvisEffect : Effect
 {
-    private Renderer[] rends;
     private float animation_duration = 0.5f;
     private float alphaValue = 0.3f; // how transparent does your team see you
     public InvisEffect(Player _target, float duration) : base(_target)
     {
         this.duration = duration;
-        
         this.name = "INVIS_EFFECT";
-        this.rends = _target.GetComponentsInChildren<Renderer>();
     }
 
     public override void OnEffectApplied()
     {        
         _target.GetComponent<Animator>().SetBool("IsInvisible", true);
-        Debug.Log("Inivisble start");
-        // for (int i = 0; i < this.rends.Length; i++)
-        // {
-        //     Renderer rend = rends[i];
-        //     if (_target.GetTeam() == PlayerController.LocalInstance.GetPlayer().GetTeam())
-        //     {
-        //         // if same team, appear transparent
-        //         rend.material.SetFloat("_alphaValue", alphaValue);
-        //     }
-        //     else
-        //     {
-        //         // if enemy team, appear invisible
-        //         rend.material.SetFloat("_alphaValue", 0f);
-        //     }
-        // }
     }
 
     public override void UpdateEffect()
     {
-        // if (age <= animation_duration)
-        // {
-        //     float progress = age / animation_duration;
 
-        //     for (int i = 0; i < this.rends.Length; i++)
-        //     {
-        //         Renderer rend = rends[i];
-        //         rend.material.SetFloat("_dissolved", progress);
-        //     }
-        // }
-        // else if (age >= duration - animation_duration)
-        // {
-        //     float progress = (age - (duration - animation_duration)) / animation_duration;
-        //     for (int i = 0; i < this.rends.Length; i++)
-        //     {
-        //         Renderer rend = rends[i];
-        //         rend.material.SetFloat("_dissolved", 1-progress);
-        //     }
-        // }
     }
-
 
     public override void OnEffectEnd()
     {   
         _target.GetComponent<Animator>().SetBool("IsInvisible", false);
-        // for (int i = 0; i < this.rends.Length; i++)
-        // {
-        //     Renderer rend = rends[i];
-        //     rend.material.SetFloat("_alphaValue", 1.0f);
-        // }
     }
 
 }
