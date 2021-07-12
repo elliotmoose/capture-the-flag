afedd0d188de0f231692e55f04a752df7c2cb023
elliotmoose <kyzelliot@gmail.com>
2021-07-12 17:47:09 +0800

    reorganised materials folder, added adept EMP animation

--- Assets/Src/Skills/Slow.cs
+++ Assets/Src/Skills/Slow.cs
@@ -8,7 +8,10 @@ public class Slow : Skill
     private float radius = 15.0f; // radius of effect
     private float percentageDecrease = 3.0f;
     private float duration = 2.0f;
+    private string animation = "EMP";
+    private Animator animator;
 
+    private LocalPlayer caster;
     public Slow()
     {
         cooldown = 8.0f;
@@ -17,16 +20,46 @@ public class Slow : Skill
 
     public override void UseSkill(LocalPlayer player)
     {
+        caster = player;
+        this.animator = player.GetComponent<Animator>();
+        caster.OnAnimationStart += OnAnimationStart;
+        caster.OnAnimationCommit += OnAnimationCommit;
+        caster.OnAnimationRelease += OnAnimationRelease;
+        caster.OnAnimationEnd += OnAnimationEnd;
+        caster.SetDisabled(true);
+        animator.SetBool("IsEMPing", true);
+    }
+
+    public void OnAnimationStart(string animationName) {
+        if(animationName != animation) return;
+        Debug.Log("Animation started: " + animation);
+    }
+
+    public void OnAnimationCommit(string animationName) {
+        if(animationName != animation) return;
+        Trigger();
+    }
+
+    public void OnAnimationRelease(string animationName) {
+        if(animationName != animation) return;
+        caster.SetDisabled(false);
+    }
+    
+    public void OnAnimationEnd(string animationName) {
+        if(animationName != animation) return;
+        animator.SetBool("IsEMPing", false);
+    }
+    private void Trigger() {
         Debug.Log(name + " skill is used");
-        Collider[] hitColliders = Physics.OverlapSphere(player.transform.position, radius);
+        Collider[] hitColliders = Physics.OverlapSphere(caster.transform.position, radius);
         
         foreach (Collider c in hitColliders)
         {
             LocalPlayer target = c.gameObject.GetComponent<LocalPlayer>();
                 
-            if (target != null && player != target)
+            if (target != null && caster.team != target.team)
             {
-                target.TakeNetworkEffect(EffectType.Slow, player.OwnerClientId);
+                target.TakeNetworkEffect(EffectType.Slow, caster.OwnerClientId);
                 // SlowEffect effect = new SlowEffect(target, percentageDecrease, duration);
                 // target.TakeEffect(effect);
             }            
