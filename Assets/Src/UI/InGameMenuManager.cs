57c4a2611b03782430de4d1fafbdde33ae6fae86
elliotmoose <kyzelliot@gmail.com>
2021-07-29 18:22:52 +0800

    Added leave game logic

--- Assets/Src/UI/InGameMenuManager.cs
+++ Assets/Src/UI/InGameMenuManager.cs
@@ -38,6 +38,6 @@ public class InGameMenuManager : MonoBehaviour
     }
 
     public void LeaveGame() {
-        // SceneTransitionManager.Instance.GameToMainMenu();
+        SceneTransitionManager.Instance.GameToMainMenu();
     }
 }
